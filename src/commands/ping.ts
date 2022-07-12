b58e80a7c06232a9a9d6c4bf722f52084abac4d0
LeBogo <lebogomc@gmail.com>
2022-07-12 13:43:28 +0200

    Removed unnecessary commands

--- src/commands/ping.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
-
-export default {
-    builder: new SlashCommandBuilder().setDescription("Replies with Pong!"),
-    run: function (_interaction: CommandInteraction) {
-        return "Pingeling, Pongelong, Dingeling, Dongelong!";
-    },
-};
