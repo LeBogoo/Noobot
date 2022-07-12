f74e7e004a3e604c7074d2320bc0a297e04e36d3
LeBogo <lebogomc@gmail.com>
2022-07-12 14:14:00 +0200

    Added remove command

---
+++ src/commands/remove.ts
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { ApplicationCommand, CommandInteraction } from "discord.js";
+import { isCustomCommand, PATHS } from "../helper";
+import { unlinkSync } from "fs";
+import { BotCommand } from "../types";
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+export default {
+    builder: new SlashCommandBuilder()
+        .setDescription("Removes a custom command.")
+        .addStringOption((option) =>
+            option.setName("name").setDescription("The name of the command that should be removed.").setRequired(true)
+        ),
+
+    run: async function (interaction: CommandInteraction) {
+        const name = interaction.options.getString("name")?.toLocaleLowerCase() || "defaultname";
+
+        if (!isCustomCommand(name)) return `Command \`${name}\` doesnt exist!`;
+
+        const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+
+        const res = await rest.get(
+            Routes.applicationGuildCommands(interaction.client.application?.id, interaction.guild?.id)
+        );
+        let command = res.find((cmd: { name: string }) => cmd.name == name);
+
+        await rest.delete(
+            Routes.applicationGuildCommands(interaction.client.application?.id, interaction.guild?.id) +
+                "/" +
+                command.id
+        );
+
+        unlinkSync(`${PATHS.CUSTOM_COMMANDS}/${name}.json`);
+
+        return `Command \`${name}\` removed!`;
+    },
+} as unknown as BotCommand;
