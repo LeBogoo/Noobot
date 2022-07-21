f5328da4f1e5649d94231a357e1a61d3cb2c26ba
LeBogo <lebogomc@gmail.com>
2022-07-21 19:03:14 +0200

    Renamed messageContextHandler

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,6 +1,6 @@
 import { Client, Interaction } from "discord.js";
 import commandHandler from "../handlers/commandHandler";
-import messageContextHandler from "../handlers/messageContextHandler";
+import messageContextHandler from "../handlers/messageContextMenuHandler";
 import modalHandler from "../handlers/modalHandler";
 
 export default function (_client: Client, interaction: Interaction) {
