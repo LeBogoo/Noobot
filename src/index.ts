39ad557e695196783a752779a070713ab7abbd26
LeBogo <lebogomc@gmail.com>
2022-07-25 17:53:09 +0200

    Fixed error handling.

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,37 @@ dotenv.config();
 
 export const logger = new Logger("../logs");
 
-try {
-    console.log(Intents.FLAGS);
 
 
 
-    const client = new Client({
-        intents: [
-            Intents.FLAGS.GUILDS,
-            Intents.FLAGS.GUILD_MEMBERS,
-            Intents.FLAGS.GUILD_BANS,
-            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-            Intents.FLAGS.GUILD_VOICE_STATES,
-            Intents.FLAGS.GUILD_PRESENCES,
-            Intents.FLAGS.GUILD_MESSAGES,
-            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        ],
-    });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
+});
 
-    /**
-     * Register all events listed in 'src/events' dir.
-     */
-    readdirSync("./events").forEach(async (file) => {
-        const event = (await import("./events/" + file)).default;
-        const eventName = file.split(".")[0];
-        client.on(eventName, (...args: unknown[]) => event(client, ...args));
-    });
+/**
+ * Register all events listed in 'src/events' dir.
+ */
+readdirSync("./events").forEach(async (file) => {
+    const event = (await import("./events/" + file)).default;
+    const eventName = file.split(".")[0];
+    client.on(eventName, (...args: unknown[]) => event(client, ...args));
+});
 
-    client.login(process.env.TOKEN);
-} catch (error: unknown) {
-    logger.log(`An error occured: ${error}`);
-}
+client.login(process.env.TOKEN);
+
+process.on('uncaughtException', (error) => {
+    logger.log(`An error occured: ${error.stack}`);
+})
+
+setTimeout(() => {
+    throw new Error("Hello, error!")
+}, 10000);
