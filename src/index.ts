9fc8a2c3bb988bf12b26d341f327cba2dd7c40d5
LeBogo <lebogomc@gmail.com>
2022-07-25 17:43:39 +0200

    Updated intents

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,20 @@ dotenv.config();
 export const logger = new Logger("../logs");
 
 try {
+    console.log(Intents.FLAGS);
+
+
+
     const client = new Client({
         intents: [
             Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_BANS,
+            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILD_PRESENCES,
             Intents.FLAGS.GUILD_MESSAGES,
             Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-            Intents.FLAGS.GUILD_MEMBERS,
-            Intents.FLAGS.DIRECT_MESSAGES,
         ],
     });
 
